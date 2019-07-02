64f32e98f416bba01c9896ac276ac83b2dc461bd
tgalvin001 <tgalvin001@gmail.com>
2019-07-02 17:08:00 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RSLogixPowerSupply
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
