a8f75be2d434bf89c51e9027ddda632b2256092c
tgalvin001 <tgalvin001@gmail.com>
2019-07-05 13:54:47 -0500

    Print PWS off messages

--- PowerSupply.cs
+++ PowerSupply.cs
@@ -6,10 +6,33 @@ namespace RSLogixPowerSupply
 {
     public class PowerSupply
     {
-        public List<int> NodeList = new List<int>();
+        public List<String> NodeList = new List<String>();
         public String powerSupplyNumber = null;
-        public int network = 0;
+        public String network = null;
         public String PDPFeed = null;
         public String ACFeed = null;
+
+        public void AddNodeToPowerSupply(string[] nodes)
+        {
+            foreach(var s in nodes)
+            {
+                NodeList.Add(s);
+            }
+            
+        }
+
+        public void PrintNodeList()
+        {
+            if (NodeList.Count < 1)
+                Console.WriteLine("No nodes");
+            else
+            {
+                foreach (var i in NodeList)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,18 +7,6 @@ namespace RSLogixPowerSupply
     class Program
     {
 
-       
-            private static void displayOne()
-            {
-            Console.WriteLine("Do something funny");
-            }
-
-            public int displayTwo()
-            {
-                return 10;
-            }
-       
-
         private enum MainMenu : int
         { 
             AddItem = 1,
@@ -30,7 +18,8 @@ namespace RSLogixPowerSupply
         private enum addItemMenu : int
         {
             AddPowerSupply = 1,
-            BackToMainMenu = 2
+            addNodes = 2,
+            BackToMainMenu = 3
         }
 
         private static readonly string MainMenuText =
@@ -45,14 +34,26 @@ namespace RSLogixPowerSupply
             "Add Item Menu\n" +
             "--------------\n" +
             "\t1 - Add Power Supply\n" +
-            "\t2 - Go back to main menu\n" +
+            "\t2 - Add Nodes\n" +
+            "\t3 - Go back to main menu\n" +
             "Your Option: ";
 
         private void functionOneSpecific()
         {
             Console.WriteLine("IN private function");
         }
-
+        private static void  PrintOffList(List<PowerSupply> t)
+        {
+            foreach(var i in t)
+            {
+                Console.WriteLine($"PWS{i.powerSupplyNumber} , AC{i.ACFeed}, PDP{i.PDPFeed} Network{i.network}");
+                foreach(var j in i.NodeList)
+                {
+                    Console.Write($"XIO DNet{i.network}.Node[{j}].OK ");
+                }
+                Console.Write($"OTE IP67.PWS{i.powerSupplyNumber}_OFF");
+            }
+        }
 
 
         static void Main(string[] args)
@@ -60,15 +61,6 @@ namespace RSLogixPowerSupply
             bool endApp = false;
             MainMenu MainMenuAction;
             addItemMenu addItemMenuAction;
-            displayOne();
-            PowerSupply PWS1 = new PowerSupply();
-            PWS1.network = 3;
-            PWS1.NodeList.Add(2);
-            PWS1.NodeList.Add(13);
-            foreach (var i in PWS1.NodeList)
-            {
-                Console.WriteLine($"Network {PWS1.network} Node:{i}");
-            }
             List<PowerSupply> PowerSupplyList = new List<PowerSupply>();
 
             while (!endApp)
@@ -93,7 +85,7 @@ namespace RSLogixPowerSupply
 
                         if (!Enum.TryParse(Console.ReadLine(), out addItemMenuAction))
                             Console.WriteLine("I do not understand your option");
-                        else if (addItemMenuAction == addItemMenu.AddPowerSupply)
+                        if (addItemMenuAction == addItemMenu.AddPowerSupply)
                         {
                             Console.Write("What is the PWS number: ");
                             PowerSupply newPowerSupply = new PowerSupply();
@@ -102,23 +94,42 @@ namespace RSLogixPowerSupply
                             newPowerSupply.PDPFeed = Console.ReadLine();
                             Console.Write("Which AC leg is the PWS fed from: ");
                             newPowerSupply.ACFeed = Console.ReadLine();
+                            Console.Write("Which Network does this PWS feed: ");
+                            newPowerSupply.network = Console.ReadLine();
+
                             PowerSupplyList.Add(newPowerSupply);
 
 
                         }
+                        else if (addItemMenuAction == addItemMenu.addNodes)
+                        {
+                            if (PowerSupplyList.Count == 0)
+                            {
+                                Console.WriteLine("No PowerSupplies in List. Click 1 to add a PowerSupply");
+                            }
+                            else
+                            {
+                                Console.Write("Which Power Supply do you want to attach Nodes to: ");
+                                string ID = Console.ReadLine();
+                                int index = PowerSupplyList.FindIndex(r => r.powerSupplyNumber == ID);
+                                if(index != -1)
+                                {
+                                    Console.WriteLine("Which Node to add: ");
+                                    PowerSupplyList[index].AddNodeToPowerSupply(Console.ReadLine().Split(' '));
+                                    
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Power Supply not found");
+                                }
+   
+                            }
+                           
+                        }
                         else
                         {
                             enditemMenu = true;
-                            foreach (var i in PowerSupplyList)
-                            {
-                                Console.WriteLine(i.powerSupplyNumber);
-                            }
                         }
-
-
-
-
-
                     }
                     while (!enditemMenu);
 
@@ -130,6 +141,8 @@ namespace RSLogixPowerSupply
                 if (MainMenuAction == MainMenu.Exit)
                 {
                     Console.WriteLine("Goodbye!");
+                    PrintOffList(PowerSupplyList);
+
                     endApp = true;
                 }
 
